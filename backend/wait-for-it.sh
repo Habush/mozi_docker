3faa16c760beef0a6f146d6881467687e7f68516
Enku  Wendwosen <enku.wende@gmail.com>
2018-08-17 15:33:53 +0200

    -Add jenkins Docker, Update guile file to add persistence dependencies

--- backend/wait-for-it.sh
+++ backend/wait-for-it.sh
@@ -7,7 +7,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD="MOZI_DB_SEC" psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD="MOZI_DB_SEC" psql -h "$host" -U "mozi_adm" -d "mozi_db" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
